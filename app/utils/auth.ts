c0987c035db0b3953cb843049c0f769cbef6a4a4
Negar <negar.karimnejad@gmail.com>
2023-12-16 20:09:24 +0330

    coding

---
+++ app/utils/auth.ts
@@ -0,0 +1,28 @@
+import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import prisma from "@/app/utils/db";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+   
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD,
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM,
+    // }),
+  ],
+} satisfies NextAuthOptions;

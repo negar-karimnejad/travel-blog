f0cc92c48f3a6dde00affe0a623e47642a2985e7
Negar <negar.karimnejad@gmail.com>
2023-12-18 21:41:03 +0330

    fixed bug

--- hooks/UseMenuActive.tsx
+++ hooks/UseMenuActive.tsx
@@ -1,11 +1,12 @@
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
-export default function useMenuActive(route: any) {
+const useMenuActive = (route: any) => {
   const router = useRouter();
   const pathname = usePathname();
 
   const isActive =
     (pathname.includes(route) && route.length > 1) || pathname === route;
-
   return isActive;
-}
+};
+
+export default useMenuActive;
